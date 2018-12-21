44c991f76b67fd51cee593da5cbb611238a65841
tangzhifeng11 <330200387@qq.com>
2018-12-21 11:32:09 +0800

    Initial Commit

---
+++ PayView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PayView
+//
+//  Created by mac on 2018/12/21.
+//  Copyright © 2018 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
